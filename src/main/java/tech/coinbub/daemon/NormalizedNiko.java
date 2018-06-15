71544970353261fc28e833fa71a59eaee820e1ac
Thomas Wilson <thomas@thomaspwilson.com>
2018-06-14 20:25:17 -0400

    Test wallet notifications

--- src/main/java/tech/coinbub/daemon/NormalizedNiko.java
+++ src/main/java/tech/coinbub/daemon/NormalizedNiko.java
@@ -30,6 +30,7 @@ public class NormalizedNiko implements Normalized<Niko> {
         result.height = block.height;
         result.time = block.time;
         result.previousblockhash = block.previousblockhash;
+        result.nextblockhash = block.nextblockhash;
         
         if (!block.tx.isEmpty()) {
             result.tx = new ArrayList<>();
@@ -80,16 +81,16 @@ public class NormalizedNiko implements Normalized<Niko> {
         result.comment_from = tx.comment;
         result.comment_to = tx.to;
 
-        if (!tx.details.isEmpty()) {
+        if (tx.details != null && !tx.details.isEmpty()) {
             result.details = new ArrayList<>();
-        }
-        for (TransactionDetail rawDetail : tx.details) {
-            final tech.coinbub.daemon.normalization.model.TransactionDetail detail
-                    = new tech.coinbub.daemon.normalization.model.TransactionDetail();
-            detail.address = rawDetail.address;
-            detail.amount = rawDetail.amount;
-            detail.fee = rawDetail.fee;
-            result.details.add(detail);
+            for (TransactionDetail rawDetail : tx.details) {
+                final tech.coinbub.daemon.normalization.model.TransactionDetail detail
+                        = new tech.coinbub.daemon.normalization.model.TransactionDetail();
+                detail.address = rawDetail.address;
+                detail.amount = rawDetail.amount;
+                detail.fee = rawDetail.fee;
+                result.details.add(detail);
+            }
         }
 
         return result;
